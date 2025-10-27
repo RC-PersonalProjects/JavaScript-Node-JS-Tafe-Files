4e6377fc319a6d644ed7e4edacba8add441aef33
Reece Clarke <reececlarke94@gmail.com>
2025-10-27 13:06:23 +1100

    Initial Commit 2.

---
+++ JSDOC/docThisFunction.js
@@ -0,0 +1,36 @@
+//@ ts-check
+
+/**
+ *
+ * @fileOverview This is the file overview, this is where the description of the script <i>file</i> goes!
+ * @author Reece D Clarke
+ * @version 1.0.1.3.lol.676   and stuff...
+ * @example //Here is your example:
+ * var a = 1;
+ * var b = 1;
+ *
+ */
+ 
+
+
+/**
+ * This is variable a
+ * @type {number} a
+ */
+var a = 1;
+
+/**
+ * @type {number} a
+ * @type {number} b
+ */
+var b = 1;
+
+
+function subtract(a, b)
+{
+    var c = a - b;
+    return c;
+}
+
+var x = subtract;
+console.log(x);
---
+++ text.js
@@ -0,0 +1,32 @@
+
+//@ts-check
+ 
+ /**
+  *@fileOverview <b>This is the second JSDoc Comment Block</b>
+  *@author Reece My Dude
+  */
+
+/**
+ * This involves 100
+ * @type {number} 
+ * @author Reece
+ */
+var a = 100;
+
+/**
+ * 
+ * @type {number}
+ * @description This is a thing
+ * @author my dude
+ */
+var bbb = 1;
+
+
+function subtract(a, b)
+{
+    var c = a - b;
+    return c;
+}
+
+var x = subtract;
+console.log(x);
